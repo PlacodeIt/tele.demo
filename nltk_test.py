a685b70c06b394a49b4a133b5cb43206675ecf36
EinatShealtiel <134155571+EinatShealtiel@users.noreply.github.com>
2024-05-31 14:30:12 +0300

    name

--- nltk_test.py
+++ nltk_test.py
@@ -2,6 +2,7 @@ import nltk
 from textblob import TextBlob
 # import VEDAR
 from nltk.sentiment import SentimentIntensityAnalyzer
+
 sia = SentimentIntensityAnalyzer()
 # VADER is a lexicon-based sentiment analyzer
 # that uses a dictionary of words and phrases
@@ -15,17 +16,19 @@ nltk.download('vader_lexicon')
 
 # sample texts
 messages = [
- "dear @verizonsupport your service is straight 💩 in dallas.. been with y'all over a decade and this is all time low for y'all. i'm talking no internet at all.",
- "@verizonsupport I sent you a dm",
- "thanks to michelle et al at @verizonsupport who helped push my no-show-phone problem along. Order canceled successfully, and I ordered this for pickup today at the Apple store in the mall."
- ]
+    "dear @verizonsupport your service is straight 💩 in dallas.. been with y'all over a decade and this is all time low "
+    "for y'all. i'm talking no internet at all.",
+    "@verizonsupport I sent you a dm",
+    "thanks to michelle et al at @verizonsupport who helped push my no-show-phone problem along. Order canceled "
+    "successfully, and I ordered this for pickup today at the Apple store in the mall. "
+]
 
 # sentiment analysis using TextBlob
 for telegram_msg in messages:
-  blob = TextBlob(telegram_msg)
-  print(blob.sentiment)
+    blob = TextBlob(telegram_msg)
+    print(blob.sentiment)
 
 # sentiment analysis using VEDAR
 for telegram_msg in messages:
-  scores = sia.polarity_scores(telegram_msg)
-  print(scores)
+    scores = sia.polarity_scores(telegram_msg)
+    print(scores)
