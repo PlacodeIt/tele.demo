ff9d6ba6ffdaa5bdc15b9d9b9662d0c3ed60aa1e
PlatoCode <115954277+PlacodeIt@users.noreply.github.com>
2024-06-01 16:51:08 +0300

    first connection with mongod ^_^

--- search.py
+++ search.py
@@ -16,28 +16,27 @@ async def search_for_word_in_query(word, query):
     for channel_id, channel_title in channels_list:
         print(f"@@@@ Searching in {channel_title}...")
         async for message in client.iter_messages(channel_id, search=word, limit=15):
-            print(f"Found 'zionists' in message {message.id} in {channel_title}")  # : {message.text}")
+            print(f"Found '{word}' in message {message.id} in {channel_title}")  # : {message.text}")
             messages_list.append({
                 "message_text": message.text,
                 "message_id": message.id,
                 "chat_title": channel_title,
                 "chat_id": channel_id,
-                "chat_username": message.sender.username,
+                "chat_username": message.sender.username if message.sender else None,
                 "message_link": f"https://t.me/{channel_title}/{message.id}"
             })
+            # Add message to the database
             add_message(message.text, message.id, channel_title, channel_id, message.sender.username if message.sender else None)
-            return channels_list, messages_list
 
+    return channels_list, messages_list
 
 async def main():
     query = "gaza"
     word = "zionists"
 
-    # await search_for_zionists_in_channels(query)
     messages = await search_for_word_in_query(word, query)
-    for message in messages:
+    for message in messages[1]:  # messages[1] contains the messages_list
         print(message)
-
 
 if __name__ == "__main__":
     client.loop.run_until_complete(main())
