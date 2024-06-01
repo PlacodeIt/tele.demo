4bbfd051e1c776dbd1c4dbf9cb57fe3bfe974893
EinatShealtiel <134155571+EinatShealtiel@users.noreply.github.com>
2024-06-01 15:56:39 +0300

    some unnecessary changes

--- channel_fetcher.py
+++ channel_fetcher.py
@@ -1,15 +1,11 @@
 from telethon import TelegramClient, events, sync, functions, types
 import asyncio
 import configparser
-
 config = configparser.ConfigParser()
 config.read('config.ini')
-
-api_id = 20195144  # config['telegram']['api_id'] https://my.telegram.org/
-api_hash = "1df6d7ef94ce3d1dbb45bcc65e6443d6"  # config['telegram']['api_hash']
-
-client = TelegramClient('tele1', int(api_id), api_hash)
-
+api_id = config['telegram']['api_id']
+api_hash = config['telegram']['api_hash']
+client = TelegramClient('tele1', api_id, api_hash)
 
 async def fetch_channels_with_query(query):
     """ Fetch channels that match the query."""
