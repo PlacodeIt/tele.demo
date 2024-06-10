5540fa5dcbadb8992ddb27cfdefa82b8f11638d4
PlatoCode <115954277+PlacodeIt@users.noreply.github.com>
2024-06-10 13:40:56 +0300

    Update with AuthPage
    blabla

--- react/src/components/auth/AuthPage.js
+++ react/src/components/auth/AuthPage.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import './AuthPage.css';
+import { Button, TextField, Checkbox, FormControlLabel, Typography, Container, CssBaseline, Link, Grid } from '@material-ui/core';
 
 const AuthPage = () => {
   const [isSignUp, setIsSignUp] = useState(false);
@@ -27,53 +29,106 @@ const AuthPage = () => {
   };
 
   return (
-    <div>
-      <h2>{isSignUp ? 'Sign Up' : 'Log In'}</h2>
-      {isSignUp && (
-        <input
-          type="email"
-          placeholder="Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className="auth-container">
+        <Typography component="h1" variant="h5" className="auth-title">
+          {isSignUp ? 'Sign Up' : 'Log In'}
+        </Typography>
+        {isSignUp && (
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email"
+            name="email"
+            autoComplete="email"
+            autoFocus
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="auth-input"
+          />
+        )}
+        <TextField
+          variant="outlined"
+          margin="normal"
+          required
+          fullWidth
+          id="username"
+          label="Username"
+          name="username"
+          autoComplete="username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          className="auth-input"
         />
-      )}
-      <input
-        type="text"
-        placeholder="Username"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-      />
-      <input
-        type="password"
-        placeholder="Password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      {!isSignUp && (
-        <>
-          <label>
-            <input
-              type="checkbox"
-              checked={rememberMe}
-              onChange={(e) => setRememberMe(e.target.checked)}
+        <TextField
+          variant="outlined"
+          margin="normal"
+          required
+          fullWidth
+          name="password"
+          label="Password"
+          type="password"
+          id="password"
+          autoComplete="current-password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          className="auth-input"
+        />
+        {!isSignUp && (
+          <>
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" checked={rememberMe} onChange={(e) => setRememberMe(e.target.checked)} />}
+              label="Remember me"
             />
-            Remember me
-          </label>
-          <button onClick={handleLogIn}>Log In</button>
-          <p>
-            Forgot my password | <span onClick={() => setIsSignUp(true)} style={{ cursor: 'pointer', color: 'blue' }}>Sign Up</span>
-          </p>
-        </>
-      )}
-      {isSignUp && (
-        <>
-          <button onClick={handleSignUp}>Sign Up</button>
-          <p>
-            Already have an account? <span onClick={() => setIsSignUp(false)} style={{ cursor: 'pointer', color: 'blue' }}>Log In</span>
-          </p>
-        </>
-      )}
-    </div>
+            <Button
+              fullWidth
+              variant="contained"
+              color="primary"
+              onClick={handleLogIn}
+              className="auth-button"
+            >
+              Log In
+            </Button>
+            <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="body2">
+                  Forgot password?
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link href="#" variant="body2" onClick={() => setIsSignUp(true)} className="auth-link">
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
+            </Grid>
+          </>
+        )}
+        {isSignUp && (
+          <>
+            <Button
+              fullWidth
+              variant="contained"
+              color="primary"
+              onClick={handleSignUp}
+              className="auth-button"
+            >
+              Sign Up
+            </Button>
+            <Grid container>
+              <Grid item>
+                <Link href="#" variant="body2" onClick={() => setIsSignUp(false)} className="auth-link">
+                  {"Already have an account? Log In"}
+                </Link>
+              </Grid>
+            </Grid>
+          </>
+        )}
+      </div>
+    </Container>
   );
 };
 
