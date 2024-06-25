364b99d82d898917ad665c1b7f8c7ce6a2009e7d
PlatoCode <115954277+PlacodeIt@users.noreply.github.com>
2024-06-25 19:04:46 +0300

    mainscreen
    mainscreen

---
+++ react/src/components/design/Authbox.tsx
@@ -0,0 +1,13 @@
+/*
+import React, { useState } from 'react';
+import AuthService from '../services/AuthService.js';
+import { useNavigate } from 'react-router-dom';
+import './AuthPage.css';
+import { Container, CssBaseline, Typography, TextField, Grid, Link } from '@mui/material';
+
+const authstyle = {} => {
+    return {
+
+    }
+}
+export default authstyle;*/
--- react/src/components/screens/MainScreen.js
+++ react/src/components/screens/MainScreen.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Container, CssBaseline, Typography } from '@mui/material';
 import { NavBar } from '../design/NavBar.tsx'
+import {channelTable}
 
 
 const MainScreen = () => {
