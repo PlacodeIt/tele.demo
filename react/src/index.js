e33e2d2bd0f882be447dbfd1fb406d7ca5bba8bc
PlatoCode <115954277+PlacodeIt@users.noreply.github.com>
2024-07-04 20:24:53 +0300

    react query client update
    react query client update

--- react/src/index.js
+++ react/src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import App from './App';
+
+
+const queryClient = new QueryClient();
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
     <App />
+    </QueryClientProvider>
   </React.StrictMode>
 );
